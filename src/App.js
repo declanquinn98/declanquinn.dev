12bc2e77dd2af6c08ab35a82278109ce5f689737
Declan <dquinn133131@gmail.com>
2022-04-10 21:05:30 +1000

    iFrame test

--- src/App.js
+++ src/App.js
@@ -19,19 +19,7 @@ const App = () => {
 
 const Home = () => {
     return (
-        <div
-            style={{
-                width: "100vw",
-                height: "100vh",
-                color: Colors.textBlack,
-                backgroundColor: Colors.natural30,
-            }}
-        >
-            <a href='https://declanquinn.dev/collective'>Boneyard Collective</a>
-            {/* <Greeting />
-            <Portfolio /> */}
-
-        </div>
+        <iframe style={{ width: '100vw', height: '200vh' }} src="https://boneyardcollective.gatsbyjs.io" title="Boneyard Collective"></iframe>
     );
 }
 
